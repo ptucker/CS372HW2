08d06c936797a4a92f21c9509be7d53026bb10af
Peter Tucker <ptucker@whitworth.edu>
2019-01-09 14:19:23 -0800

    feedback

--- PersonClassHW2/src/Person.java
+++ PersonClassHW2/src/Person.java
@@ -14,7 +14,7 @@ abstract class Person {
     public String getName() {return name;}
     public String getPhoneNumber() {return phoneNumber;}
     public int getAge() {return age;}
-    public void setAge(int age) { this.age = age; }
+    public void setAge(int age) { this.age = age; } //PT -- make sure you get a valid age, phone number. -2
     public void setName(String name) { this.name = name; }
     public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 }
